77521e6a293a8bc2fefd0b84a1c3104978e082b8
CzeschinW <wilrczeschin@gmail.com>
2022-02-05 15:32:01 -0600

    commit

--- Module_3/functions.php
+++ Module_3/functions.php
@@ -1,17 +1,17 @@
 <?php
 //Deal a "card" with a random value between 1 and 10, and return the value
 function drawMain(){
-    int card;
+    $card = 0;
 
-    card = random_int(1, 10);
+    $card = random_int(1, 10);
 
-    return card;
+    return $card;
 }
 
 function drawSide(){
     $cards = [];
 
-    for(int i = 0; i < 3; i++){
+    for($i = 0; $i < 3; $i++){
         $cards[] = random_int(-5, 5);
     }
     return $cards;
